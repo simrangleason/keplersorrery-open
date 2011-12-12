c11aae48406813f5ae3dd0c455ed7f0f530b18c3
Simran <fermatslasttheremin@yahoo.com>
2011-12-11 23:23:39 -0800

    small change to run script

--- run_keplers_orrery.sh
+++ run_keplers_orrery.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-java kepler.Kepler --undecorated --canvas 760 --time 90 --loop --shuffle   --playlist worlds/playlists/symmetries.kpl --playlist worlds/playlists/physics.kpl --playlist worlds/playlists/exercises.kpl --playlist worlds/playlists/music.kpl
+java kepler.Kepler --undecorated --canvas 760 --kiosk --time 90 --loop --shuffle   --playlist worlds/playlists/symmetries.kpl --playlist worlds/playlists/physics.kpl --playlist worlds/playlists/exercises.kpl --playlist worlds/playlists/music.kpl
